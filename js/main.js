e179414504ced09bc72bf3c6f5f5c5071374426c
seoyeongjeong <gkeorgl@gmail.com>
2022-05-17 11:45:39 +0900

    20220517-3

--- js/main.js
+++ js/main.js
@@ -261,6 +261,34 @@ if ($(window).width() < 750) {
 }
 })//모바일 서비스 닫기
 
+//모바일 네비
+
+$('ul li:has(ul)').addClass('has-submenu');
+$('ul li ul').addClass('sub-menu');
+$('ul.dropdown li').hover(function () {
+    $(this).addClass('hover');
+}, function () {
+    $(this).removeClass('hover');
+});
+var $menu = $('#menu'), $menulink = $('#spinner-form'), $search = $('#search'), $search_box = $('.search_box'), $menuTrigger = $('.has-submenu > a');
+$menulink.click(function (e) {
+    $menulink.toggleClass('active');
+    $menu.toggleClass('active');
+    if ($search.hasClass('active')) {
+        $('.menu.active').css('padding-top', '50px');
+    }
+});
+$search.click(function (e) {
+    e.preventDefault();
+    $search_box.toggleClass('active');
+});
+$menuTrigger.click(function (e) {
+    e.preventDefault();
+    var t = $(this);
+    t.toggleClass('active').next('ul').toggleClass('active');
+});
+$('ul li:has(ul)');
+
 
 
 })//ready
