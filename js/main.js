111eb5e0e26f526aa8cb9cb2d8dbba2025e564d1
seoyeongjeong <gkeorgl@gmail.com>
2022-05-17 12:35:37 +0900

    20220517-5

--- js/main.js
+++ js/main.js
@@ -263,31 +263,20 @@ if ($(window).width() < 750) {
 
 //모바일 네비
 
-$('ul li:has(ul)').addClass('has-submenu');
-$('ul li ul').addClass('sub-menu');
-$('ul.dropdown li').hover(function () {
-    $(this).addClass('hover');
-}, function () {
-    $(this).removeClass('hover');
-});
-var $menu = $('#menu'), $menulink = $('#spinner-form'), $search = $('#search'), $search_box = $('.search_box'), $menuTrigger = $('.has-submenu > a');
+var $menu = $('#menu'), $menulink = $('#spinner-form'),  $menuTrigger = $('.has-submenu > a');
 $menulink.click(function (e) {
     $menulink.toggleClass('active');
     $menu.toggleClass('active');
-    if ($search.hasClass('active')) {
-        $('.menu.active').css('padding-top', '50px');
-    }
 });
-$search.click(function (e) {
-    e.preventDefault();
-    $search_box.toggleClass('active');
-});
+$('.menu > ul > li').click(function(){
+ // $(this).toggleClass('has-submenu');
+  $(this).find('.menu-open').toggleClass('active')
+})
 $menuTrigger.click(function (e) {
     e.preventDefault();
     var t = $(this);
-    t.toggleClass('active').next('ul').toggleClass('active');
-});
-$('ul li:has(ul)');
+    t.toggleClass('active')
+}); 
 
 
 
